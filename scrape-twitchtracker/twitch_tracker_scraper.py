40a4d7cbbab33c702c9e240e07ebed13719b6024
matthewyu01 <56417255+matthewyu01@users.noreply.github.com>
2020-05-09 01:50:28 -0700

    Added actual twitch_tracker_scraper.py

---
+++ scrape-twitchtracker/twitch_tracker_scraper.py
@@ -0,0 +1,30 @@
+import urllib.request 
+import csv
+
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup 
+
+
+def main():
+
+    web_url = urllib.request.urlopen(link_request)
+    soup = BeautifulSoup(web_url, 'lxml')
+    viewers = soup.find_all('td', class_ = ["to-number", "to-monthyear"]) 
+
+    length = len(viewers)
+
+
+    with open('twitch-monthly-viewers.csv', 'w+', newline='') as csv_file:
+        csv_write = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        csv_write.writerow(['Date' + ',' + 'Average Viewers'])
+        for x in range(92, length, 4):
+            csv_write.writerow([     str(viewers[x].text).strip('\n'),   str(viewers[x+1].text).strip('\n')])
+
+
+link = "https://twitchtracker.com/statistics"
+link_request = urllib.request.Request(link, headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}) 
+
+
+if __name__ == "__main__":
+    main()
+  
