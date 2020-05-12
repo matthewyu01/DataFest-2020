1e0de5d2d622cc4e99fe07334dde092d2243070b
matthewyu01 <56417255+matthewyu01@users.noreply.github.com>
2020-05-12 15:57:12 -0700

    Finished

--- scrape-twitchtracker/improved_twitch_tracker_scraper.py
+++ scrape-twitchtracker/improved_twitch_tracker_scraper.py
@@ -1,24 +1,18 @@
 import pandas as pd 
 import requests 
-import csv
-
-from bs4 import BeautifulSoup 
 
 def main():
-    link = "https://twitchtracker.com/statistics"
+    link = 'https://twitchtracker.com/statistics'
     data_list = pd.read_html(requests.get(link, headers={'User-agent': 'Mozilla/5.0'}).text)
 
     df = data_list[0]
-
-    # truncate dataframe to begin on Jan 2019
-    recent_data = df.truncate(before = 76, after = 92)
-    # Modifies indices to start at 0
-    recent_data.index = list(range(17))
+    
+    recent_data = df.truncate(before = 76, after = 92) # truncate dataframe to begin on Jan 2019
+    recent_data.index = list(range(17)) # Modifies indices to start at 0
     
     recent_data.to_csv('twitch-adv-data.csv',  index=False)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
   
 
